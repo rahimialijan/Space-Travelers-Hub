63907cd216a7726f19ccd028720a642ed7d881cf
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-06-27 13:37:26 +0500

    Display the rocket data on the page

--- src/router/rockets.js
+++ src/router/rockets.js
@@ -1,15 +1,26 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getRocketsData } from '../Redux/rockets/rocketSlice';
+import RocketItem from '../components/rockets';
 
 const Rockets = () => {
+  const { rocketData, isLoading, error } = useSelector(
+    (store) => store.rockets,
+  );
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getRocketsData());
   }, [dispatch]);
-  return (
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+  return isLoading ? (
+    <div>Data is loading...</div>
+  ) : (
     <div>
-      <h1>Rockets</h1>
+      {rocketData.map((data) => (
+        <RocketItem key={data.id} data={data} />
+      ))}
     </div>
   );
 };
